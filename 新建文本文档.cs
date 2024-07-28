89b30863367924d9100e757c7efc225ed31e0c5a
zxs <1376260231@qq.com>
2024-07-28 14:33:48 +0800

    add test

---
+++ 新建文本文档.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Diagnostics;
+using System.Text;
+
+public class Solution
+{
+
+    //88
+    public static void Merge(int[] nums1, int m, int[] nums2, int n)
+    {
+        for (int i = m; i < nums1.Length; i++)
+        {
+            nums1[i] = nums2[i - m];
+        }
+
+        Array.Sort(nums1);
+
+    }
+    static void Main(string[] args)
+    {
+        
+
+    }
+}
